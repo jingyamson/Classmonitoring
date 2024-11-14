61820e6d2d30b45647865bdadead25c93a836a22
Jaycee Capulong <capulongako16@gmail.com>
2024-11-14 09:39:43 +0800

    Update script.blade.php

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -16,6 +16,7 @@
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@1.16.1/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
 <!-- Additional JS plugins -->
 <script src="{{ asset('assets/js/vartical-layout.min.js') }}"></script>
